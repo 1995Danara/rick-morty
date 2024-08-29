1305b7cd1e6698618c5aa7a7d3e8d17f983f2dd2
Danara Dmitrieva <dmitrievadanara@gmail.com>
2024-08-29 16:21:33 +0300

    update

--- src/components/Characters/Query.ts
+++ src/components/Characters/Query.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client'
 
 export const GET_CHARACTERS = gql`
-  query {
-    characters {
+  query GetCharacters($search: String!) {
+    characters(filter: { name: $search }) {
       results {
         id
         name
